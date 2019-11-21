9eecae7f46eb98290bfe0deee7be8e66770c3b3e
Renata <renata.sendreti@gmail.com>
2019-11-20 22:27:49 -0300

    Implementação de múltiplos obstáculos com posição inicial aleatória. Início da implementação de indicação de GAME OVER.

--- main.c
+++ main.c
@@ -17,21 +17,25 @@ GLfloat panX = 0.0;
 GLfloat panY = -25.0;
 GLfloat width_wall = 9.0;
 GLfloat taxa_descida_inimigo = 0;
-GLfloat pos_inimigo = 0;
-
-GLfloat x0_inimigo = 0;
-GLfloat y0_inimigo = 500; // = janelaY
 
 int tiro = 0;
 int contTiro = 0;
 
+int fimDeJogo = 0;
 
+float auxTempo = 0.0;
 
+int tempoPadraoObjetos = 50; //50
+int tempoIntervaloObjetos = 90; //90
+int tempoCriaObjetos = 0.0;
+int tempoRolagem = 10;
 
 GLuint texture_id1, texture_id2;
-//Objeto objetos[100];
+
+Lista objetos;
 Aeronave aviao;
 
+// Define posições iniciais da Aeronave e dos tiros
 void inicializaAeronave(){
   aviao.x = 0.0;
   aviao.y = -25.0;
@@ -46,6 +50,7 @@ void inicializaAeronave(){
   }
 }
 
+// Inicialização
 void init(void) {
   
   glClearColor(0.3f,0.0f,1.0f,1.0f);
@@ -58,8 +63,9 @@ void init(void) {
           (-win), (+win),
           (-win), (win));
   // Ortho só deve ser setado uma vez, e nunca deve ser alterado
+  
   inicializaAeronave();
-  pos_inimigo = 25.0;
+  inicializarLista(&objetos);
   //lighting(); //defininido os parâmetros de iluminação
 
   glGenTextures(1,&texture_id1);
@@ -67,6 +73,7 @@ void init(void) {
   glPushMatrix();
 }
 
+// Desenha avião e tiros
 void aeronave(){
   //glMatrixMode(GL_MODELVIEW);
   //glLoadIdentity();
@@ -90,33 +97,68 @@ void aeronave(){
 
 }
 
-void display(void){
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpa o buffer
-  aeronave();
-  //desenharInimigo(15.0, (panY+10.0));
-    int i;
-   for(i=0; i<NUM_TIROS; i++){
-    if(dist(15.0,pos_inimigo, aviao.tiros[i].x, aviao.tiros[i].y) < 5){
-      pos_inimigo = 37.0;
-      aviao.tiros[i].x =-31.0;
+// Verifica se ocorreu a colisão tiro-obstaculo e aviao-obstaculo
+void verificarColisao(){
+  int i;
+  Objeto *p = objetos.inicio;
+  while(p != NULL){
+    for(i=0; i<NUM_TIROS; i++){
+      //if(dist(15.0,pos_inimigo, aviao.tiros[i].x, aviao.tiros[i].y) < 5){
+      if(dist(p->posX, p->posY, aviao.tiros[i].x, aviao.tiros[i].y) < 4){
+        //pos_inimigo = 37.0;
+        p->posY = 37.0;
+        remover(&objetos, p->posY, p->posX);// = 37.0;
+        aviao.tiros[i].x =-31.0;
+      }
     }
-  }
-  if(dist(15.0,pos_inimigo, aviao.x, aviao.y) < 5){
-      pos_inimigo = 37.0;
+    //if(dist(15.0,pos_inimigo, aviao.x, aviao.y) < 5){
+    if(dist(p->posX, p->posY, aviao.x, aviao.y) < 4){
+      //pos_inimigo = 37.0;
+      //p->posY = 37.0;
+      fimDeJogo = 1;
       aviao.x = 0.0;
     }
-  if(pos_inimigo < -30){
-    pos_inimigo = 37.0;
+    if(p->posY < -30){
+      p->posY = 37.0;
+    }
+    p = p->prox;
   }
-  desenharInimigo(15.0,pos_inimigo);
-  glEnable(GL_TEXTURE_2D);
-  desenhaParedeEsquerda(win,width_wall,texture_id1);
-  desenhaParedeDireita(win,width_wall,texture_id1);
-  desenhaAgua(win,width_wall,texture_id2);
-  glDisable(GL_TEXTURE_2D);
+
+}
+
+// Desenha objetos obstáulo
+void desenharObjetos(){
+  Objeto *p = objetos.inicio;
+  while(p != NULL){
+    desenharInimigo(p->posX, p->posY);
+    p = p->prox;
+  }
+}
+
+// Define o que será mostrado na tela
+void display(void){
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpa o buffer
+  if(!fimDeJogo){ //DESENHA OBJETOS NA TELA
+    aeronave();
+    //desenharInimigo(15.0, (panY+10.0));
+    desenharObjetos();
+    //int posx = (2*win-win/2);
+    //desenharInimigo(rand() % posx,pos_inimigo);
+    glEnable(GL_TEXTURE_2D);
+    desenhaParedeEsquerda(win,width_wall,texture_id1);
+    desenhaParedeDireita(win,width_wall,texture_id1);
+    desenhaAgua(win,width_wall,texture_id2);
+    glDisable(GL_TEXTURE_2D);
+  }
+  else{ // TELA AZUL
+    if(auxTempo == 0.0) printf("fim de jogo\n");
+    if(auxTempo<100) auxTempo+=1;
+    else fimDeJogo = 0;
+  }
   glutSwapBuffers();
 }
 
+// NÃO EXCLUIR - Controle extra, tá sobrando, mas vamos usar pra fazer o avião rotacionar na movimentação
 void teclado(unsigned char key, int x, int y){
   //anglex = acionarLetra(key, anglex);
   switch(key){
@@ -138,60 +180,78 @@ void teclado(unsigned char key, int x, int y){
   glutPostRedisplay();
 }
 
+// Adiciona um tiro no vetor de tiros e seta a posição inicial dele
 void controleTiros(){
   aviao.tiros[contTiro].x = aviao.x;
   aviao.tiros[contTiro].y = -22.0;
-  printf("%d: %f\n",contTiro, aviao.x );
+  //printf("%d: %f\n",contTiro, aviao.x );
   //aviao.tiros[contTiro].naTela = 1;
   contTiro = (contTiro+1) % NUM_TIROS;     //troca de 10 para 30 porque se dermos tiros consecutivos sem parar porque 
                                     //com % 10 eles somem no meio da tela
   tiro = 1;
-  
 }
 
+// Controle das setas
 void controle(int key, int xx, int yy){
   /*struct Coordenadas coordenadas = acionarSetas(key, panX, panY, win, width_wall);
   panX = coordenadas.panX;
   panY = coordenadas.panY;*/
   switch(key){
         case GLUT_KEY_UP:
-            
             controleTiros();
             break;
         case GLUT_KEY_DOWN:
             //rY += 5;
             break;
         case GLUT_KEY_LEFT:
-
-            if(aviao.x-4>-win+width_wall) 
+            //if(aviao.x-4>-win+width_wall)
+            if(aviao.x  - 4<=-win +width_wall + 0.5)
+              fimDeJogo = 1;
+            else
               aviao.x -= 1.0; //movimenta o avião para a esquerda
             break;
         case GLUT_KEY_RIGHT:
-            if(aviao.x+4<win-width_wall) 
+            //if(aviao.x+4<win-width_wall) 
+            if(aviao.x + 4 >= win -width_wall - 0.5) 
+              fimDeJogo = 1;
               aviao.x += 1.0; //movimenta o avião para a direita
             break;
     }
     glutPostRedisplay();
 }
 
-void movimentarObjetosSecundarios () {
-  for (int i = 0; i < 100; i++) {
-
-    // procurar elementos dentro ?
-    // diminuir o Y (aproximar do jogador) ?
+// Cria novos objetos na lista de objetos, a partir do limite superior da tela
+void criarObjetosSecundarios(){
+  // Cria considerando o intervalo de tempo controlado pelas variáveis tempoCriaObjetos e tempoPadraoObjetos
+  if(tempoCriaObjetos == tempoPadraoObjetos){
+    inserir(&objetos, win, 28.0, width_wall);
+    tempoCriaObjetos = -tempoIntervaloObjetos;
   }
 }
 
+// Movimenta para baixo os objetos que não foram atingidos
+void movimentarObjetosSecundarios () {
+  Objeto *p = objetos.inicio;
+  while(p != NULL){
+    if(p->posY != 37.0) p->posY -= 0.2;
+    p = p->prox;
+  }
+}
+
+// Executa a cada 10ms inicialmente, cria e movimenta os objetos, além de verificar a colisão
+// TO DO: fazer variar o tempo de acordo com o nivel que o jogador está
 void movimentarPorTempo (){
-  // TODO movimentar objetos "secundários"
+  tempoCriaObjetos += 1;
+  //printf("%d\n", tempoCriaObjetos);
+  
+  criarObjetosSecundarios();
   movimentarObjetosSecundarios();
-  // TODO calcular colisao
-  // TODO
-  pos_inimigo -= 0.08;
+  verificarColisao();
+  
   if(tiro){
       int i;
       for(i=0; i<NUM_TIROS; i++){
-          aviao.tiros[i].y += 0.2;
+          aviao.tiros[i].y += 0.4;
           if(aviao.tiros[i].y > 30.0){
             aviao.tiros[i].y = -30.0;
             aviao.tiros[i].x = -31.0;
@@ -199,18 +259,18 @@ void movimentarPorTempo (){
       }
   }
   
-
   glutPostRedisplay();
-	glutTimerFunc(10, movimentarPorTempo, 1);
+	glutTimerFunc(tempoRolagem, movimentarPorTempo, 1);
 }
 
-int  main(int argc, char** argv){
+int main(int argc, char** argv){
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   glutInitWindowSize(janelaX, janelaY);
   glutInitWindowPosition(300 ,100);
   glutCreateWindow("River Raid 3D");
   init();
+
   CarregaTextura("grama.bmp",texture_id1);
   CarregaTextura("agua.bmp",texture_id2);
 
@@ -221,9 +281,7 @@ int  main(int argc, char** argv){
   glutDisplayFunc(display);
   glutKeyboardFunc(teclado);
   glutSpecialFunc(controle);
-  glutTimerFunc(10, movimentarPorTempo, 1);
-
-  
+  glutTimerFunc(tempoRolagem, movimentarPorTempo, 1);
 
   glutMainLoop();
 
--- util.c
+++ util.c
@@ -24,56 +24,92 @@ typedef struct Coordenadas{
   GLfloat panY;
 }Coordenadas;
 
-/*
 typedef struct Objeto{
   GLfloat posX;
   GLfloat posY;
   int id;
   int tipo;
-  struct Objeto *prox;
+  struct Objeto *prox, *ant;
 }Objeto;
 
 typedef struct Lista{
   struct Objeto *inicio, *fim;
 }Lista;
 
-void inicializaLista(Lista *lista){
+// Inicializa a lista com NULL
+void inicializarLista(Lista *lista){
   lista->inicio = NULL;
   lista->fim = NULL;
 }
 
-int verificaUnicidadeCoordenadas(Lista *lista, float x, float y){
-  //verificar se o mesmo objeto nao esta sendo colocado no mesmo lugar que outro já existente
-  //talvez seja bom considerar se ele não está em um lugar muito próximo, inclusive
-
-  //return 1 //se for única
-  //return 0 //caso contrário
+// Imprime lista para conferência, auxiliar
+void imprimeLista(Lista *lista){
+  Objeto *p = lista->inicio;
+  while(p!=NULL){
+    printf("(%.2f, %.2f)\n", p->posX, p->posY);
+    p = p->prox;
+  }
 }
 
-void insere(Lista *lista, int winX, int winY){
+// Verificar se o mesmo objeto nao esta sendo colocado no mesmo lugar que outro já existente
+int verificarUnicidadeCoordenadas(Lista *lista, float x, float y){
+
+  if(lista->inicio != NULL){
+    Objeto *percorre = lista->inicio;
+    while(percorre != NULL){
+      if(percorre->posX == x && percorre->posY == y){
+        return 0; //já existe objeto com a mesma posição (é meio impossivel, função tá inutil)
+      }
+      percorre = percorre->prox;
+    }
+  }
+  return 1; //posição é unica
+
+}
+
+// Insere um novo objeto na lista
+void inserir(Lista *lista, int winX, int winY, int width_wall){
   struct Objeto *novo = malloc(sizeof(Objeto));
   novo->prox = NULL;
 
-  do {
-    //id pode ser a tupla da posição:
-    novo->posX = rand() % (2*winX) - winX; //reconsiderar quando as paredes forem corretamente desenhadas
-    novo->posY = rand() % (2*winY) - winY;
+  do{
+    novo->posX = (rand() % (2*winX - 2*width_wall)) - winX + width_wall; //calcula pra setar uma posição x aleatória dentro da parte azul
+    novo->posY = winY;
     //novo->tipo = //será definido o tipo do objeto, para que saibamos qual é a dimensão considerada para a colisão
-  }while(verificaUnicidadeCoordenadas(lista, novo->posX, novo->posY));
+  }while(!verificarUnicidadeCoordenadas(lista, novo->posX, novo->posY));
   
-  if(lista->inicio == NULL)
+  if(lista->inicio == NULL){
+    novo->ant = NULL;
     lista->inicio = novo;
-  else
+  }
+  else{
+    novo->ant = lista->fim;
     lista->fim->prox = novo;
+  }
   lista->fim = novo;
+  imprimeLista(lista);
 }
 
-void remove(Lista *lista){
+// Remove o objeto que tem as posições enviadas por parâmetro da lista de objetos
+void remover(Lista *lista, int posX, int posY){
+  if(lista->inicio != NULL){
+    Objeto *percorre = lista->inicio;
 
-}*/
+    while(percorre != NULL){ //procura o objeto para remoção
+      if(percorre->posX == posX && percorre->posY == 37.0){
+        percorre->ant->prox = percorre->prox;
+        percorre->prox->ant = percorre->ant;
+        free(percorre);
+        return;
+      }
+      percorre = percorre->prox;
+    }
+  }
+}
 
+// Calcula a distância entre dois pontos (px, py) e (qx, qy)
 float dist(GLfloat px, GLfloat py, GLfloat qx, GLfloat qy){
-    printf("px %2f py %2f qx %2f qy %2f \n", px, py, qx, qy);
-    printf("dist %f\n", sqrt(pow((px - qx), 2) + pow((py - qy), 2)) );
+    //printf("px %2f py %2f qx %2f qy %2f \n", px, py, qx, qy);
+    //printf("dist %f\n", sqrt(pow((px - qx), 2) + pow((py - qy), 2)) );
     return sqrt(pow((px - qx), 2) + pow((py - qy), 2));
 }
